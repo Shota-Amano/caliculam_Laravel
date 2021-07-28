0d928517722b8d6d68c0384b2545c0f25c5a1de4
Shota-Amano <ab18004@shibaura-it.ac.jp>
2021-07-28 18:45:02 +0900

    [add]保存処理実行_残り

--- blog/resources/views/create.blade.php
+++ blog/resources/views/create.blade.php
@@ -15,11 +15,13 @@
                 @csrf
                 <div>
                     <h2>タイトル</h2>
-                    <input type="text" name="post[title]" placeholder="タイトル"/>
+                    <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                    <p class="body__error" style="color:red">{{ $errors->first('post.title') }}</p>
                 </div>
                 <div>
                     <h2>本文</h2>
-                    <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。" ></textarea>
+                    <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。" >{{ old('post.body') }}</textarea>
+                    <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
                 </div>
                 <input type="submit" value="保存"/>
             </form>
