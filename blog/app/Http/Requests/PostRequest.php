ff6b86e5816fdbd85aa0fe3a1d829f8fadaeb846
Shota-Amano <ab18004@shibaura-it.ac.jp>
2021-07-28 22:20:31 +0900

    よくわからない２

--- blog/app/Http/Requests/PostRequest.php
+++ blog/app/Http/Requests/PostRequest.php
@@ -9,7 +9,7 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            'post.title' => 'required|string|max:100',
+            'post.title' => 'required|string|max:40',
             'post.body' => 'required|string|max:4000',
         ];
     }
